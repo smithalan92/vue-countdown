0dde97c685863986f9c780a728681c7bce9218ee
Alan Smith <alan.smith@teamwork.com>
2020-10-24 17:23:46 +0100

    Imporve bundle build size by removing babel/corejs

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,9 +7,6 @@ module.exports = {
     'plugin:vue/vue3-essential',
     '@vue/airbnb',
   ],
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
